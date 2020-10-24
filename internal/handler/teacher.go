82e100a30ff8f199c95dc601942f5ce38291053c
André Gaspar <andre.gaspar@pismo.io>
2020-10-24 14:45:25 -0300

    (user#17) Handler done

--- internal/handler/teacher.go
+++ internal/handler/teacher.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"encoding/json"
 	"github.com/getclasslabs/go-tools/pkg/request"
 	"github.com/getclasslabs/go-tools/pkg/tracer"
+	"github.com/getclasslabs/user/internal/service/teacher"
 	"github.com/gorilla/mux"
 	"net/http"
+	"strconv"
 )
 
 func SearchTeacherById(w http.ResponseWriter, r *http.Request) {
@@ -12,8 +15,15 @@ func SearchTeacherById(w http.ResponseWriter, r *http.Request) {
 	i.TraceIt(spanName)
 	defer i.Span.Finish()
 
-	id := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(mux.Vars(r)["id"])
+	if err != nil{
+		i.LogError(err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
 
+	t, err := teacher.GetTeacherById(i, id)
 
-
+	ret, _ := json.Marshal(t)
+	_, _ = w.Write(ret)
 }
